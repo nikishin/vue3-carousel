508e627d0bdefd3539fe6f13816447098834695f
nkostya <nikishin@d-a-c.ru>
2026-02-09 15:24:21 +0300

    [!] Сборка

--- dist/carousel.cjs
+++ dist/carousel.cjs
@@ -479,9 +479,10 @@ function useWheel(options) {
         // Determine scroll direction
         const deltaY = Math.abs(event.deltaY) > threshold ? event.deltaY : 0;
         const deltaX = Math.abs(event.deltaX) > threshold ? event.deltaX : 0;
+        // eslint-disable-next-line no-console
+        console.log('Z', vertical.value, deltaY >= deltaX);
         // preventDefault if scroll by config axis
-        if ((vertical.value && deltaY >= deltaX)
-            || (!vertical.value && deltaY <= deltaX)) {
+        if ((vertical.value && deltaY >= deltaX) || (!vertical.value && deltaY <= deltaX)) {
             event.preventDefault();
         }
         // If neither delta exceeds the threshold, don't navigate
